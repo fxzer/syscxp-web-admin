75c2ec373b0c62f7317a6c21fe3d555004955249
fxzer <wy3010099292@163.com>
2023-08-18 09:41:44 +0800

    perf: 优化编辑器使用说明样式

--- src/views/EditorHelp.vue
+++ src/views/EditorHelp.vue
@@ -1,29 +1,29 @@
 <template>
-  <el-drawer :visible="visible" direction="rtl" size="450px" @close="close" :show-close="true"
+  <el-drawer :visible="visible" direction="rtl" size="520px" @close="close" :show-close="true"
     :wrapperClosable="true">
     <h2 style="margin-left: 20px;">使用说明</h2> 
     <div style="padding:20px">
       <el-collapse v-model="activeName" accordion>
         <el-collapse-item title="情景一：添加新文章" name="1" >
-        <div style="margin-left:20px;">
-          <h4>
-            1. 使用<a href="https://www.365editor.com/" target="_blank">365编辑器</a> 排版文章，并保存至草稿箱
-          </h4>
-          <el-image style="width: 200px; height: 200px" src="https://s1.ax1x.com/2023/08/17/pP11TRP.jpg"
-            :preview-src-list="srcListSave">
-          </el-image>
+          <div style="margin-left:20px;">
+            <h4>
+              1. 使用<a href="https://www.365editor.com/" target="_blank">365编辑器</a> 排版文章，并保存至草稿箱
+            </h4>
+            <el-image style="width: 200px; height: 200px" src="https://s1.ax1x.com/2023/08/17/pP11TRP.jpg"
+              :preview-src-list="srcListSave">
+            </el-image>
 
-          <h4>
-            2. 复制全文，粘贴到编辑区
-          </h4>
-          <el-image style="width: 200px; height: 200px" src="https://s1.ax1x.com/2023/08/17/pP117xf.png"
-            :preview-src-list="srcListCopy">
-          </el-image>
+            <h4>
+              2. 复制全文，粘贴到编辑区
+            </h4>
+            <el-image style="width: 200px; height: 200px" src="https://s1.ax1x.com/2023/08/17/pP117xf.png"
+              :preview-src-list="srcListCopy">
+            </el-image>
 
-          <h4>
-            3. 发布
-          </h4>
-        </div>
+            <h4>
+              3. 发布
+            </h4>
+          </div>
         </el-collapse-item>
         <el-collapse-item title="情景二：加入公众号已有文章" name="2">
           <div style="margin-left:20px;">
@@ -55,7 +55,7 @@
         <el-collapse-item title="情景三：更新文章" name="3">
           <div style="margin-left:20px;">
           <h4>
-            1. 去<a href="https://www.365editor.com/" target="_blank">365编辑器</a> 草稿箱更新文章
+            1. 在<a href="https://www.365editor.com/" target="_blank">365编辑器</a> 草稿箱更新文章
           </h4>
           <el-image style="width: 200px; height: 200px" src="https://s1.ax1x.com/2023/08/17/pP11TRP.jpg"
             :preview-src-list="srcListSave">
@@ -111,6 +111,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+::v-deep .el-drawer__header{
+  margin-bottom:0px;
+}
+::v-deep .el-collapse-item__header{
+  font-size: 16px;
+  &.is-active{
+    color:#409EFF;
+  }
+}
 h4{
   margin-bottom: 5px;
 }
